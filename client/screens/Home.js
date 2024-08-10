26a9ac9cd13ab17ba923b323d742692c4cf0c759
Shashank Yadav <54665960+Skshashank@users.noreply.github.com>
2024-08-11 03:07:34 +0530

    Footer Menu and Vector-icons/fontawesome

--- client/screens/Home.js
+++ client/screens/Home.js
@@ -1,16 +1,26 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React, { useContext } from "react";
 import { AuthContext } from "@/context/authContext";
+import FooterMenu from "@/components/Menus/FooterMenu";
 
 const Home = () => {
   //glbal state destructure , we will use context and pass Auth context
   const [state] = useContext(AuthContext);
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Home Sweet Home </Text>
       <Text>{JSON.stringify(state, null, 4)}</Text>
+      <FooterMenu />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    margin: 10,
+  },
+});
 
 export default Home;
