3dd3a59036c98d0f39f1aed04e32e5f144159194
Shashank Yadav <54665960+Skshashank@users.noreply.github.com>
2024-08-12 05:08:11 +0530

    20 account final update config

--- client/screens/Account.js
+++ client/screens/Account.js
@@ -1,47 +1,97 @@
-import { View, Text, StyleSheet, Image, TextInput } from "react-native";
-import React, { useContext } from "react";
-import { AuthContext } from "@/context/authContext";
-import FooterMenu from "@/components/Menus/FooterMenu";
-
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  Touchable,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../context/authContext";
+import FooterMenu from "../components/Menus/FooterMenu";
+import axios from "axios";
 const Account = () => {
-  const [state] = useContext(AuthContext);
+  //global state
+  const [state, setState] = useContext(AuthContext);
+  const { user, token } = state;
+  //local state
+  const [name, setName] = useState(user?.name);
+  const [password, setPassword] = useState(user?.password);
+  const [email] = useState(user?.email);
+  const [loading, setLoading] = useState(false);
+
+  //handle update user data
+  const handleUpdate = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.put("/auth/update-user", {
+        name,
+        password,
+        email,
+      });
+      setLoading(false);
+      let UD = JSON.stringify(data);
+      setState({ ...state, user: UD?.updatedUser });
+      alert(data && data.message);
+    } catch (error) {
+      alert(error.response.data.message);
+      setLoading(false);
+      console.log(error);
+    }
+  };
   return (
     <View style={styles.container}>
-      <View style={{ alignItems: "center" }}>
-        <Image
-          source={{
-            uri: "https://static.vecteezy.com/system/resources/thumbnails/008/259/265/small_2x/young-smiling-man-cartoon-character-keeping-arms-crossed-businessman-standing-with-folded-arms-pose-flat-illustration-isolated-on-white-background-free-vector.jpg",
-          }}
-          style={{ height: 200, width: 200, borderRadius: 100 }}
-        />
-      </View>
-      <Text style={styles.warningtext}>
-        Currenlty you can only Update your Name and Password*
-      </Text>
-      <View style={styles.inputContainer}>
-        <Text style={styles.inputText}>Name</Text>
-        <TextInput style={styles.inputBox} value={state?.user.name} />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.inputText}>Email</Text>
-        <TextInput
-          style={styles.inputBox}
-          value={state?.user.email}
-          editable={false}
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.inputText}>Password</Text>
-        <TextInput style={styles.inputBox} value={state?.user.password} />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.inputText}>Role</Text>
-        <TextInput
-          style={styles.inputBox}
-          value={state?.user.role}
-          editable={false}
-        />
-      </View>
+      <ScrollView>
+        <View style={{ alignItems: "center" }}>
+          <Image
+            source={{
+              uri: "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png",
+            }}
+            style={{ height: 200, width: 200, borderRadius: 100 }}
+          />
+        </View>
+        <Text style={styles.warningtext}>
+          Currently You Can Only Update Your Name And Password*
+        </Text>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputText}>Name</Text>
+          <TextInput
+            style={styles.inputBox}
+            value={name}
+            onChangeText={(text) => setName(text)}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputText}>Email</Text>
+          <TextInput style={styles.inputBox} value={email} editable={false} />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputText}>Password</Text>
+          <TextInput
+            style={styles.inputBox}
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            secureTextEntry={true}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputText}>Role</Text>
+          <TextInput
+            style={styles.inputBox}
+            value={state?.user.role}
+            editable={false}
+          />
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <TouchableOpacity style={styles.updateBtn} onPress={handleUpdate}>
+            <Text style={styles.updateBtnText}>
+              {loading ? "Please Wait" : "Update Profile"}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
       <View style={{ flex: 1, justifyContent: "flex-end" }}>
         <FooterMenu />
       </View>
@@ -52,14 +102,14 @@ const Account = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "space-between",
     margin: 10,
+    justifyContent: "space-between",
+    marginTop: 40,
   },
   warningtext: {
     color: "red",
     fontSize: 13,
     textAlign: "center",
-    marginTop: 8,
   },
   inputContainer: {
     marginTop: 20,
@@ -76,10 +126,23 @@ const styles = StyleSheet.create({
     width: 250,
     backgroundColor: "#ffffff",
     marginLeft: 10,
-    borderRadius: 6,
     fontSize: 16,
     paddingLeft: 20,
+    borderRadius: 5,
+  },
+  updateBtn: {
+    backgroundColor: "black",
+    color: "white",
+    height: 40,
+    width: 250,
+    borderRadius: 10,
+    marginTop: 30,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  updateBtnText: {
+    color: "#ffffff",
+    fontSize: 16,
   },
 });
-
 export default Account;
