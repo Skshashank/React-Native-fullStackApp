62ef6783cb2370fbdbe04a8b79c70f1818d84d8b
Shashank Yadav <54665960+Skshashank@users.noreply.github.com>
2024-08-11 17:02:02 +0530

    navigation, screen menu created

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,34 +1,6 @@
 import React from 'react';
-import Login from '@/screens/auth/Login';
-import Register from '../screens/auth/Register';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {AuthProvider } from '@/context/authContext';
-import Home from '@/screens/Home';
-
-const Stack = createNativeStackNavigator();
+import RootNavigation from '@/navigation';
 
 export default function Index() {
-  return (
-    <NavigationContainer independent={true}>
-      <AuthProvider>
-      <Stack.Navigator initialRouteName='Login'>
-        <Stack.Screen 
-        name='Home' 
-        component={Home} 
-        options={{ headerShown: false }} />
-        <Stack.Screen 
-        name='Register' 
-        component={Register} 
-        options={{ headerShown: false }} />
-        <Stack.Screen 
-          name='Login' 
-          component={Login} 
-          options={{ headerShown: false }} 
-        />
-      </Stack.Navigator>
-      </AuthProvider>
-      
-    </NavigationContainer>
-  );
+  return <RootNavigation />;
 }
