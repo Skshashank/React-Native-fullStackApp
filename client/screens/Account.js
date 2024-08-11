3383193a36a6f35218a8713630eea90c5219d78e
Shashank Yadav <54665960+Skshashank@users.noreply.github.com>
2024-08-12 03:32:35 +0530

    20 - account page config- user icon added, name, email, password,role , for update

--- client/screens/Account.js
+++ client/screens/Account.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, TextInput } from "react-native";
 import React, { useContext } from "react";
 import { AuthContext } from "@/context/authContext";
 import FooterMenu from "@/components/Menus/FooterMenu";
@@ -7,9 +7,41 @@ const Account = () => {
   const [state] = useContext(AuthContext);
   return (
     <View style={styles.container}>
-      <Text>NAME : {state?.user.name}</Text>
-      <Text>EMAIL : {state?.user.email}</Text>
-      <Text>ROLE : {state?.user.role}</Text>
+      <View style={{ alignItems: "center" }}>
+        <Image
+          source={{
+            uri: "https://static.vecteezy.com/system/resources/thumbnails/008/259/265/small_2x/young-smiling-man-cartoon-character-keeping-arms-crossed-businessman-standing-with-folded-arms-pose-flat-illustration-isolated-on-white-background-free-vector.jpg",
+          }}
+          style={{ height: 200, width: 200, borderRadius: 100 }}
+        />
+      </View>
+      <Text style={styles.warningtext}>
+        Currenlty you can only Update your Name and Password*
+      </Text>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputText}>Name</Text>
+        <TextInput style={styles.inputBox} value={state?.user.name} />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputText}>Email</Text>
+        <TextInput
+          style={styles.inputBox}
+          value={state?.user.email}
+          editable={false}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputText}>Password</Text>
+        <TextInput style={styles.inputBox} value={state?.user.password} />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputText}>Role</Text>
+        <TextInput
+          style={styles.inputBox}
+          value={state?.user.role}
+          editable={false}
+        />
+      </View>
       <View style={{ flex: 1, justifyContent: "flex-end" }}>
         <FooterMenu />
       </View>
@@ -22,6 +54,31 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "space-between",
     margin: 10,
+  },
+  warningtext: {
+    color: "red",
+    fontSize: 13,
+    textAlign: "center",
+    marginTop: 8,
+  },
+  inputContainer: {
+    marginTop: 20,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputText: {
+    fontWeight: "bold",
+    width: 70,
+    color: "gray",
+  },
+  inputBox: {
+    width: 250,
+    backgroundColor: "#ffffff",
+    marginLeft: 10,
+    borderRadius: 6,
+    fontSize: 16,
+    paddingLeft: 20,
   },
 });
 
